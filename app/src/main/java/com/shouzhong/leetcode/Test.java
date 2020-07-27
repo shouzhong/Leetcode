3d1e4ecce9ad7d91c3453bd0195072e99015706f
shouzhong <wuyifeng_12@163.com>
2020-07-27 08:50:10 +0800

    2020-07-27

--- app/src/main/java/com/shouzhong/leetcode/Test.java
+++ app/src/main/java/com/shouzhong/leetcode/Test.java
@@ -9,26 +9,28 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 public class Test {
-    public String countAndSay(int n) {
-        if (n < 1) return "";
-        if (n == 1) return "1";
-        String s = countAndSay(n - 1);
-        StringBuilder sb = new StringBuilder();
-        int len = s.length();
-        char c = s.charAt(0);
-        int count = 1;
-        for (int i = 1; i < len; i++) {
-            char temp = s.charAt(i);
-            if (temp == c) count++;
-            else {
-                sb.append(count).append(c);
-                c = temp;
-                count = 1;
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) return "0";
+        int len1 = num1.length();
+        int len2 = num2.length();
+        int[] res = new int[len1 + len2];
+        for (int i = len1 - 1; i >= 0; i--) {
+            int n1 = num1.charAt(i) - '0';
+            for (int j = len2 - 1; j >= 0; j--) {
+                int n2 = num2.charAt(j) - '0';
+                int sum = res[i + j + 1] + n1 * n2;
+                res[i + j + 1] = sum % 10;
+                res[i + j] += sum / 10;
             }
         }
-        sb.append(count).append(c);
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < res.length; i++) {
+            if (i == 0 && res[i] == 0) continue;
+            sb.append(res[i]);
+        }
         return sb.toString();
     }
 }
---
+++ app/src/main/java/com/shouzhong/leetcode/最大子序和.kt
@@ -0,0 +1,16 @@
+package com.shouzhong.leetcode
+
+/**
+ * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。
+ *
+ */
+fun maxSubArray(nums: IntArray): Int {
+    val len = nums.size
+    var result = nums[0]
+    var sum = 0
+    for (i in 0 until len) {
+        sum = Math.max(sum + nums[i], nums[i])
+        result = Math.max(result, sum)
+    }
+    return result
+}

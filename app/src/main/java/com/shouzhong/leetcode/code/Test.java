84801f6f5fa27a9a4c20441c33c3d6390972cda1
shouzhong <wuyifeng_12@163.com>
2020-08-06 08:38:43 +0800

    2020-08-06

--- app/src/main/java/com/shouzhong/leetcode/code/Test.java
+++ app/src/main/java/com/shouzhong/leetcode/code/Test.java
@@ -1,47 +1,25 @@
 package com.shouzhong.leetcode.code;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.Stack;
-import java.util.stream.Collectors;
-
 public class Test {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> edges = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) {
-            edges.add(new ArrayList<Integer>());
-        }
-        int[] visited = new int[numCourses];
-        for (int[] temp : prerequisites) {
-            edges.get(temp[1]).add(temp[0]);
-        }
-        boolean[] bs = {true};
-        for (int i = 0; i < numCourses && bs[0]; i++) {
-            if (visited[i] == 0) canFinish(edges, visited, bs, i);
-        }
-        return bs[0];
+    public int rob(TreeNode root) {
+        int[] sums = robDfs(root);
+        return Math.max(sums[0], sums[1]);
     }
 
-    public void canFinish(List<List<Integer>> edges, int[] visited, boolean[] bs, int u) {
-        visited[u] = 1;
-        for (int v : edges.get(u)) {
-            if (visited[v] == 0) {
-                canFinish(edges, visited, bs, v);
-                if (!bs[0]) return;
-            } else if (visited[v] == 1) {
-                bs[0] = false;
-                return;
-            }
+    public int[] robDfs(TreeNode root) {
+        if (root == null) return new int[] {0, 0};
+        int[] l = robDfs(root.left);
+        int[] r = robDfs(root.right);
+        int selected = root.val + l[1] + r[1];
+        int notSelected = Math.max(l[0], l[1]) + Math.max(r[0], r[1]);
+        return new int[] {selected, notSelected};
+    }
+
+    public enum A {
+        INSTANCE;
+
+        public void a() {
+
         }
-        visited[u] = 2;
     }
 }
